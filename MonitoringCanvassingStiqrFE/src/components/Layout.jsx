bc941bb1d898c66ff63c4b5b2bf261a502bc7354
AL4ONE <alfianardiansyah2804@gmail.com>
2025-12-29 19:43:24 +0700

    point dari kak vio1

--- MonitoringCanvassingStiqrFE/src/components/Layout.jsx
+++ MonitoringCanvassingStiqrFE/src/components/Layout.jsx
@@ -36,7 +36,6 @@ export default function Layout({ children }) {
                     Upload
                   </Link>
                 )}
-                )}
                 {user?.role === 'supervisor' && (
                   <>
                     <Link
