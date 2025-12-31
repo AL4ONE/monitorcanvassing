d0cd0ff474195b2ce7ffc1ff2cf6d4d4681ae34a
AL4ONE <alfianardiansyah2804@gmail.com>
2025-12-31 09:27:19 +0700

    point tambahan28

--- monitoringCanvassingStiqrBE/app/Http/Controllers/MessageController.php
+++ monitoringCanvassingStiqrBE/app/Http/Controllers/MessageController.php
@@ -10,6 +10,7 @@ use App\Services\OcrService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
