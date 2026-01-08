4906dc5a961571fd0f593802e06210f4f51450e0
mas wachid <your.email@wcloud.my.id>
2026-01-08 14:43:14 +0700

    fix: update NGINX configuration paths in Dockerfile and entrypoint script

--- docker-config/entrypoint.sh
+++ docker-config/entrypoint.sh
@@ -11,7 +11,7 @@ set -e
 if [ "$NGINX_CORS_ENABLE" = "true" ]; then
   # Jika CORS_ALLOWED_ORIGINS tidak di-set, jangan generate config baru, biarkan nginx.conf bawaan yang dipakai
   NGINX_CONF_TEMPLATE="/var/www/html/docker-config/nginx.conf.template"
-  NGINX_CONF_TARGET="/etc/nginx/conf.d/default.conf"
+  NGINX_CONF_TARGET="/etc/nginx/httpd.d/default.conf"
 
   # Generate nginx.conf dari template hanya jika CORS_ALLOWED_ORIGINS di-set dan tidak kosong
   if [ -n "$CORS_ALLOWED_ORIGINS" ]; then
